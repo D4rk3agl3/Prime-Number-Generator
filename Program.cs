a60afc2fb1ef65cd47edcf483d45e2b1dee1931e
D4rk3agl3 <lln9925@northgo-college.nl>
2022-07-23 00:30:53 +0200

    Added Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double numbertocheck = 59;
+for (int i = 2; i <= numbertocheck; i++)
+{
+    if((numbertocheck % i) == 0 && i == numbertocheck){
+        Console.WriteLine(numbertocheck + " IS a prime.");
+        break;      
+    }
+    else if((numbertocheck % i) == 0 && i < numbertocheck){
+        Console.WriteLine(numbertocheck + " is NOT a prime.");
+        break;
+    }
+}
+
+    
